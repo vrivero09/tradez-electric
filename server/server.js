df26c9972f6b5ba8a9cc91b3d48a70eb706ce9e9
vrivero09 <rivero0288@outlook.com>
2020-08-05 15:32:58 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,35 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-require("dotenv").config();
-
+//const bodyParser = require('body-parser');
+const path = require('path');
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8000;
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static(publicPath));
 
-const port = process.env.PORT || 5000;
+//app.set("view engine", "ejs");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send("home");
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
-//app.use middleware
-//express static in charge of sending static files req to client
-app.use('/', express.static(path.join(__dirname, "client", "build")));
-
-//right before app.listen, add this:
-//"*" means 'catch all' route handler.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get("*", (req,res)=>{
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//   res.send("home");
+// });
+
+// app.post('/api/world', (req, res) => {
+//   console.log(req.body);
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`,
+//   );
+// });
+
+// // //app.use middleware
+// // //express static in charge of sending static files req to client
+// // app.use('/', express.static(path.join(__dirname, "client", "build")));
+
+// //right before app.listen, add this:
+// //"*" means 'catch all' route handler.
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
