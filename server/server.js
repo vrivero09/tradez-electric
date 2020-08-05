966aec201d4a0bf0bd55241305f30eb88781f049
vrivero09 <rivero0288@outlook.com>
2020-08-04 23:13:08 -0400

    test test

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
