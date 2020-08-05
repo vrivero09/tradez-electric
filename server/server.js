2f952e62e0d629a512e85538f90caea75ea86376
vrivero09 <rivero0288@outlook.com>
2020-08-05 16:03:54 -0400

    oh boy

--- server/server.js
+++ server/server.js
@@ -1,34 +1,23 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
-const publicPath = path.join(__dirname, "client", "build");
-const port = process.env.PORT || 8000;
+const publicPath = path.join(__dirname, "build");
+const port = process.env.PORT || 3000;
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(publicPath));
 
-//app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("*", (req,res)=>{
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.send("home");
-});
-
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
 
 // // //app.use middleware
 // // //express static in charge of sending static files req to client
@@ -38,4 +27,15 @@ app.post('/api/world', (req, res) => {
 // //"*" means 'catch all' route handler.
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(publicPath, "index.html"));
+// });
+
+// app.get('/', (req, res) => {
+//   res.send("home");
+// });
+
+// app.post('/api/world', (req, res) => {
+//   console.log(req.body);
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`,
+//   );
 // });
