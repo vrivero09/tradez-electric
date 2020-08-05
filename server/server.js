0b795518060f4c84671a8bcaf4e6ef86a0e4937b
vrivero09 <rivero0288@outlook.com>
2020-08-05 15:52:27 -0400

    commit

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-//app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 app.get("*", (req,res)=>{
   res.sendFile(path.join(publicPath, 'index.html'));
@@ -16,19 +16,19 @@ app.get("*", (req,res)=>{
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send("home");
-// });
+app.get('/', (req, res) => {
+  res.send("home");
+});
 
-// app.post('/api/world', (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`,
-//   );
-// });
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
 
 // // //app.use middleware
 // // //express static in charge of sending static files req to client
