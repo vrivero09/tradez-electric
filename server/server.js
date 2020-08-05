9771d162cf92208f9fca39acf58c80319e1eb7d3
vrivero09 <rivero0288@outlook.com>
2020-08-05 09:45:26 -0400

    test

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send("home");
 });
+
+app.use('/', express.static(path.join(__dirname, '/client/build')));
+
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
