383e64058a8ccd83a7e96858218c1aecee65bac1
vrivero09 <rivero0288@outlook.com>
2020-08-05 23:06:10 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use('/', express.static(path.join(__dirname, 'dist')))
 // })
 
 app.get('/home', function (req, res) {
- return res.send('I am home');
+ return res.send(console.log("Helllooo????"));
 });
 
 
