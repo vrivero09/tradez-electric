992194bd105653d9fec7b8cd06573a0cafd4f20f
vrivero09 <rivero0288@outlook.com>
2020-08-05 22:34:15 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -20,7 +20,6 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
