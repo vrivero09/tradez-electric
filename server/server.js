64a0d8ecd8b74a83f0d56c7cea3b83a24955cb6e
vrivero09 <rivero0288@outlook.com>
2020-08-05 21:48:30 -0400

    test

--- server/server.js
+++ server/server.js
@@ -18,12 +18,15 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('/home', function (req, res) {
+ return res.send('I am home');
+});
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
