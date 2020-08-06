ce89c9fca95528072a639daa464c1e4c7afda639
vrivero09 <rivero0288@outlook.com>
2020-08-05 21:27:04 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const publicPath = path.join(__dirname, "build");
-const port = process.env.PORT || 3000;
+
+
 const app = express();
+const port = process.env.PORT || 5000;
+
+const publicPath = path.join(__dirname, "build");
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(publicPath));
-
-app.set("view engine", "ejs");
-
-app.get("*", (req,res)=>{
-  res.sendFile(path.join(publicPath, 'index.html'));
+//API calls
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
 });
 
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -33,9 +49,3 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 //   res.send("home");
 // });
 
-// app.post('/api/world', (req, res) => {
-//   console.log(req.body);
-//   res.send(
-//     `I received your POST request. This is what you sent me: ${req.body.post}`,
-//   );
-// });
