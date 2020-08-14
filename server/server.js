c1b6c4dc50be30fa7853e7572e1a54045080c9d9
vrivero09 <rivero0288@outlook.com>
2020-08-14 15:38:41 -0400

    test

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+var express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const favicon = require('express-favicon');
