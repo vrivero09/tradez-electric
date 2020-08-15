3980e5e3dd36c6e8ec2e4c4c41fd94921001c989
vrivero09 <rivero0288@outlook.com>
2020-08-15 17:22:58 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 import './components/Nav';
 // import Navigation from './components/Nav';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import Home from './components/home';
 import About from './components/about';
 import Services from './components/services';
 import Contact from './components/contact';
 
 class App extends Component{
-
   render(){
-    return(
-      <BrowserRouter>
-        <div className="App">
+    const App = () => (
+      <div className="App">
+        <Switch>
           <Route exact path='/' component={Home} />
-          <Link to="/Home" />
-          {/* <IndexRoute component = {Services}/> */}
-          <Route exact path='/Home' component={Home} />
-          <Route exact path='/Services' component={Services} />
-          <Route exact path='/About' component={About} />
-          <Route exact path='/Contact' component={Contact} />
-        </div>
-      </BrowserRouter>
+          <Route path='/Home' component={Home} />
+          <Route path='/Services' component={Services} />
+          <Route path='/About' component={About} />
+          <Route path='/Contact' component={Contact} />
+        </Switch>
+      </div>
+
+    )
+    return(
+      <Switch>
+        <App/>
+      </Switch>
     )
   }
 
