c94234671ecad9f6a80ab06daad4f1a73590ffef
vrivero09 <rivero0288@outlook.com>
2020-09-13 11:22:37 -0400

    fixed scroll feature

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,10 +7,12 @@ class Home extends Component {
   render(){
     return (
         <div className="App" >
+          <Jumbotron fluid>
           <Container fluid>
             <h1 className="display-3">Marine Electric</h1>
             <p className="lead">"COMING SOON!"</p>
           </Container>
+          </Jumbotron>
         </div>
      
     );
