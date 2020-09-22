b6a7a72ec0ece97bfe7901a8c5ee95ea9484d7d4
vrivero09 <rivero0288@outlook.com>
2020-09-22 14:44:59 -0400

    added some info

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,18 +1,47 @@
 import React, { Component} from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import {  Container } from 'reactstrap';
+import { Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import Form from './Form'
 
 
-class Contact extends Component {
+class Contact extends React.Component {
+
+    handleOnClick = () => {
+        this.props.history.push("/About");
+      }
+
     render(){
+
         return(
             <div className="App">
-            <Jumbotron fluid>
-                <Container fluid>
-                    <h1 className="display-3">Contact info</h1>
-                    <p className="lead">"INFO COMING SOON!"</p>
+                <Container>
+                <Row style={{paddingTop: "100px"}}>
+                    <Col sm="6">
+                        <Card style={{ backgroundColor: "transparent" }} body >
+                        <CardTitle><h1>Contact Information</h1></CardTitle>
+                        <hr />
+                        <CardText>
+                            <ul>
+                                Phone number: 305-467-2859
+                            </ul>
+                            <ul>
+                                Location: Miami Gardens, FL
+                            </ul>
+                        </CardText>
+                        <Button color="info" onClick={this.handleOnClick}>Learn more</Button>
+                        </Card>
+                    </Col>
+
+                    <Col sm="6">
+                        <Card  style={{ backgroundColor: "transparent" }} body>
+                            <Form />
+                        </Card>
+                    </Col>
+    </Row>
                 </Container>
-            </Jumbotron>
-        </div>
+                    
+                
+      </div>
 
         );
     }
