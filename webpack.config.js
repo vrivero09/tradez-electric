a80f1cfc96328690e74e7c8091bbb69148ccdd98
vrivero09 <rivero0288@outlook.com>
2022-12-20 14:35:59 -0500

    syntax error

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development"',
+    mode: 'development',
   entry: path.resolve(__dirname, 'client/src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
