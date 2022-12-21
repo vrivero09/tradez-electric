ae543089cc9ec46f44e73c4d3ffa2bc1cdf794f1
vrivero09 <rivero0288@outlook.com>
2022-12-20 22:21:56 -0500

    include

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$|jsx/,
-        exclude: /node_modules/,
+        include: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
