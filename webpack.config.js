f9f28352a3b1ca1e79f2395a741f4097041a01cc
vrivero09 <rivero0288@outlook.com>
2022-12-20 19:53:11 -0500

    undo

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'client', 'index.js'),
+  entry: path.resolve(__dirname, 'client/src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
